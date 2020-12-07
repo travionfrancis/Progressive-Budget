04953b7f9e42d5ad5a5c4efb1dcff21ba0d7c0a0
TraviFravi <travi1500@gmail.com>
2020-12-07 15:28:57 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+// Establish PORT
+const PORT = process.env.PORT || 8080;
 const app = express();
+
 
 app.use(logger("dev"));
 
@@ -13,15 +14,18 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/columbia", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 
-// routes
 app.use(require("./routes/api.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
