9a093b2acfdcd560386eab9a770173a3a0e017e2
TraviFravi <travi1500@gmail.com>
2020-12-07 16:39:23 -0500

    adding server worker fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,43 +1,59 @@
-var CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-var urlsToCache = [
+const FILES_TO_CACHE = [
   "/",
-  "/db.js",
+  "/index.html",
   "/index.js",
-  "/manifest.json",
-  "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-512x512.png",
+  "/manifest.webmanifest",
+  "/styles.css",
 ];
 
-self.addEventListener("install", function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
-  // cache all get requests to /api routes
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
+
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(event.request)
+        return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
+              cache.put(evt.request.url, response.clone());
             }
 
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(event.request);
+            return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
     );
@@ -45,15 +61,18 @@ self.addEventListener("fetch", function(event) {
     return;
   }
 
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
+
+  evt.respondWith(
+    caches.match(evt.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request).then(response => {
+          return cache.put(evt.request, response.clone()).then(() => {
+            return response;
+          });
+        });
       });
     })
   );
